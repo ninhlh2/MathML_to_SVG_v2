4dfdd2566a690a50ece2ac166bf9d4fd25fac68a
ninhlh2 <58598732+ninhlh2@users.noreply.github.com>
2022-06-01 09:57:42 +0700

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const bodyParser = require("body-parser");
-const port = 80;
+const port = 3000;
 let MathJax = null;
 
 require('mathjax').init({
